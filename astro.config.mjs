1cbbc8e88ddf5de7896111a4160161bfb516b596
Koire <pikewb@gmail.com>
2023-09-12 11:07:58 +0900

    feat: add uploads

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
     site: "https://koire.github.io",
     base: "paikube",
-    output: "server"
+    output: "server",
+    adapter: node({
+        mode: 'standalone'
+    })
 });
